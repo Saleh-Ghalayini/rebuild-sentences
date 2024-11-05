b601595dd949385b0d36c14ddef0fbefd77d4bd6
saleh <ghalayinisaleh9@gmail.com>
2024-11-05 21:32:45 +0200

    started with the program but didnt know how to continue cus soooo tired be back later :')

---
+++ rebuild-sentences.py
@@ -0,0 +1,9 @@
+def rebuild_sentences(words, lengths):
+
+    sentence = []
+
+    for i in range(lengths):
+
+    return print(sentence)
+
+rebuild_sentences( [ "the", "sky", "is", "blue" ] , [ 3, 2, 2, 1 ] )    
