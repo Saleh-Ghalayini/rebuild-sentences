80ed99e3dc2c8e7309c7cff846edc370f623be85
saleh <ghalayinisaleh9@gmail.com>
2024-11-07 10:23:06 +0200

    program done

--- rebuild-sentences.py
+++ rebuild-sentences.py
@@ -1,9 +1,14 @@
 def rebuild_sentences(words, lengths):
 
+    #defining a new empty list that will be the sentence after the extraction of each word from the words list
     sentence = []
 
-    for i in range(lengths):
-
-    return print(sentence)
+    #looping through each word in the words list
+    for i in range(len(words)):
+            
+            #extracting the specified length in the lengths list from the words list and appending it to the sentence list
+            sentence.append(words[i][:lengths[i]])
+    #joining the values of sentence so it would be displayed as a string and adding a space between each word        
+    print(" ".join(sentence))
 
 rebuild_sentences( [ "the", "sky", "is", "blue" ] , [ 3, 2, 2, 1 ] )    
